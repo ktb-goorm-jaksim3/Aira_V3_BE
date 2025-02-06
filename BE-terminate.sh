323953a122e88219da58863199fa755772c59270
tigerlee0225 <yblee94@outlook.com>
2025-02-06 09:34:51 +0900

    commit files for ECS deployment

--- BE-terminate.sh
+++ BE-terminate.sh
@@ -4,5 +4,5 @@ docker-compose -f docker-compose.backend.yml down
 # Docker compose db remove
 docker-compose -f docker-compose.backend.yml down --volumes
 
-# Docker image remove
-docker image rm aira_v2_be-backend 
+# Docker compose image remove
+docker-compose -f docker-compose.backend.yml down --rmi local
